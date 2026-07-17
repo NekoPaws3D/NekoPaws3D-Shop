2998b8f3ba4e3ac59b517903b7b80353ba5abcb1
NekoPaws3D <dornheim-david@outlook.com>
2026-07-17 10:47:24 +0200

    Update EmailJS configuration with new keys and IDs

--- js/email-config.js
+++ js/email-config.js
@@ -4,9 +4,9 @@
 // Werte im EmailJS-Dashboard kopieren und hier einsetzen.
 // Die Public Key darf bei einer statischen Website öffentlich im Browser stehen.
 window.EMAILJS_CONFIG = {
-  publicKey: "DEINE_PUBLIC_KEY",
-  serviceId: "DEINE_SERVICE_ID",
-  contactTemplateId: "DEINE_KONTAKT_TEMPLATE_ID",
-  orderTemplateId: "DEINE_BESTELL_TEMPLATE_ID",
+  publicKey: "Nx-_bjzGMzyvQf_p0",
+  serviceId: "service_wspqhsm",
+  contactTemplateId: "template_2lm23wd",
+  orderTemplateId: "template_tk60h98",
   shopEmail: "Neko.paws3d@gmail.com"
 };
